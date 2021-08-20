4cb5c9c5f868b42af7181b7bae4717532b0f3991
Zacka Wenblade <zack2you@hotmail.com>
2021-08-20 13:11:50 +0700

    first merge_sort py

---
+++ mdrge_sort.py
@@ -0,0 +1,10 @@
+def merge_sort(list):
+    """
+    Sorts a list in ascending order
+    Return a new sorted list
+
+    Divide: Find the midpoint of the list and divide into sublists
+    Conquer: Recursively sort the sublists created in previous step
+    Combine: Merge the sorted sublists created in previous step
+    """
+    
